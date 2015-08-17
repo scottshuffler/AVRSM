e8722010f297fc944d3612ec0223ad8f251569a1
Scott <shuffleres@email.appstate.edu>
2015-08-17 17:15:21 -0400

    INIT

---
+++ SPeAK/SPeAK/SPeAK.c
@@ -0,0 +1,17 @@
+/*
+ * SPeAK.c
+ *
+ * Created: 8/17/2015 5:14:53 PM
+ *  Author: shuffleres
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
