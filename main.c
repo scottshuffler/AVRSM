010a80958399a15213e059dbe8555fdcb5af5894
Scott Shuffler <scottshuffler@gmail.com>
2015-08-25 12:00:29 -0400

    New style

---
+++ main.c
@@ -0,0 +1,15 @@
+/*
+ * SPAnKS.c
+ *
+ * Created: 8/17/2015 5:14:53 PM
+ *  Author: shuffleres
+ */ 
+
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
